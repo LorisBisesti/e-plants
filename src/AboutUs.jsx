94d798f419228407424513404224ca4141919164
LorisBisesti <loris.bisesti@gmail.com>
2025-03-18 18:21:10 +0100

    fix css About

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -7,10 +7,8 @@ function AboutUs() {
       {/* <h1 className="about-us-heading">About Us</h1> */}
       <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
       <p className="about-us-content">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
-        high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
-        more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
-        plant enthusiast.
+      At Paradise Nursery, we bring nature closer to you with a wide range of high-quality plants. Whether you're a seasoned gardener or just starting, our mission is to help you create a greener, healthier space. Explore our collection and let us help you find the perfect plant for your home or office.
+
       </p>
       <p className="about-us-content">
         Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
