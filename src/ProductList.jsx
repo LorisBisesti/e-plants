ec12f8578e1e062a58cc2b0a01514f21a11f7a1e
LorisBisesti <loris.bisesti@gmail.com>
2025-03-18 15:31:14 +0100

    Task 3: CartItems component

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,11 +7,13 @@ import CartItem from './CartItem';
 import './ProductList.css'
 
 function ProductList({ onHomeClick }) {
+
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({}); // State to store the items added to the cart
     const dispatch = useDispatch();
     const items = useSelector((state) => state.cart.items);
+    const totalQuantity = useSelector((state) => state.cart.totalQuantity);
 
     const plantsArray = [
         {
@@ -285,32 +287,51 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                        <g>
+                            {/* Cerchio rosso */}
+                            <circle
+                                cx="130"
+                                cy="125"
+                                r="30"
+                                fill="red"
+                            />
+                            {/* Testo bianco */}
+                            <text
+                                x="130"
+                                y="135"
+                                fill="white"
+                                font-size="30"
+                                text-anchor="middle"
+                            >
+                                {totalQuantity}
+                            </text>
+                        </g><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
             {!showCart ? (
                 <div className="product-grid">
-                {plantsArray.map((item, index) => (
-                    <div key={index}>
-                        <h1 className='title-card'>{item.category}</h1>
-                        <div className='product-list'>
-                            {item.plants.map((plant, pindex) => (
-                                <div className='product-card' key={pindex}>
-                                    <img className='product-image' src={plant.image} alt={plant.name} />
-                                    <div className='product-title'>{plant.name}</div>
-                                    <div className='product-price'>{plant.cost}</div>
-                                    <div>{plant.description}</div>
-                                    {!items.find((sample) => sample.name === plant.name) ? (
-                                        <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
-                                    ) : (
-                                        <h3 className='product-button added-to-cart'>Added to Cart</h3>
-                                    )}
-                                </div>
-                            ))}
+                    {plantsArray.map((item, index) => (
+                        <div key={index}>
+                            <h1 className='title-card'>{item.category}</h1>
+                            <div className='product-list'>
+                                {item.plants.map((plant, pindex) => (
+                                    <div className='product-card' key={pindex}>
+                                        <img className='product-image' src={plant.image} alt={plant.name} />
+                                        <div className='product-title'>{plant.name}</div>
+                                        <div className='product-price'>{plant.cost}</div>
+                                        <div>{plant.description}</div>
+                                        {!items.find((sample) => sample.name === plant.name) ? (
+                                            <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                        ) : (
+                                            <h3 className='product-button added-to-cart'>Added to Cart</h3>
+                                        )}
+                                    </div>
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
             )}
