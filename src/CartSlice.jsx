639b5fd1c0e374854ff72ecf352d6fa5e9fb9ef4
LorisBisesti <loris.bisesti@gmail.com>
2025-03-18 12:51:27 +0100

    Task 2: State management using Redux

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,35 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// Creazione dello slice per la gestione del carrello
 export const CreateSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
-    totalQuantity:0
+    items: [], // Array per memorizzare gli articoli nel carrello
+    totalQuantity: 0, // Quantità totale di articoli nel carrello
   },
+
   reducers: {
     addItem: (state, action) => {
-      const {name,image,cost}=action.payload;
-      const existing=state.items.find((plant)=>plant.name===name);
-      if(existing){
+      const { name, image, cost } = action.payload;
+      const existing = state.items.find((plant) => plant.name === name);
+      if (existing) {
         existing.quantity++;
-      }else{
-        state.items.push({name,image,cost,quantity:1});
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
       }
       state.totalQuantity++;
     },
     removeItem: (state, action) => {
-      let item=state.items.find((plant)=>plant.name===action.payload);
-      state.items=state.items.filter((plant)=>plant.name!==action.payload); 
-      state.totalQuantity-=item.quantity;
+      let item = state.items.find((plant) => plant.name === action.payload);
+      state.items = state.items.filter((plant) => plant.name !== action.payload);
+      state.totalQuantity -= item.quantity;
     },
     updateQuantity: (state, action) => {
-      const {name,quantity}=action.payload;
-      const existing=state.items.find((plant)=>plant.name===name);
-      if(existing){
-        let updateCost=quantity-existing.quantity;
-        existing.quantity=quantity;
-        state.totalQuantity+=updateCost;
-      } 
+      const { name, quantity } = action.payload;
+      const existing = state.items.find((plant) => plant.name === name);
+      if (existing) {
+        let updateCost = quantity - existing.quantity;
+        existing.quantity = quantity;
+        state.totalQuantity += updateCost;
+      }
     },
   },
 });
